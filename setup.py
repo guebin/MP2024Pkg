fb849dcd41e31f3a4bfab46d86fe3d9c4fce9890
Guebin Choi <81734388+guebin@users.noreply.github.com>
2024-10-31 13:44:46 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     packages=find_packages(),
     description="A package for displaying dictionary and nested structure information",
     author="Your Name",
-    author_email="guebin@jbnu.ac.kr,
+    author_email="guebin@jbnu.ac.kr",
     install_requires=[],  # 필요한 패키지 목록이 있다면 추가
 )
